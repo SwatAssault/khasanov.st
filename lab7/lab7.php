b1258adb7821a1cb8b65c538c832271595951e54
Ruslan <43928175+SwatAssault@users.noreply.github.com>
2020-11-24 09:11:50 +0500

    лаб7 закончена

--- lab7/lab7.php
+++ lab7/lab7.php
@@ -1,23 +1,43 @@
-<h1>Лабораторная работа 7</h1>
-<h2>Упражнения:</h2>
 
-<ul>
-	<li>
-		<a href="/lab7/lab7-1.html">Задание 1</a>	
-	</li>
-	<li>
-		<a href="/lab7/lab7-2.html">Задание 2</a>	
-	</li>
-	<li>
-		<a href="/lab7/lab7-3.html">Задание 3</a>	
-	</li>
-	<li>
-		<a href="/lab7/lab7-4.html">Задание 4</a>	
-	</li>
-	<li>
-		<a href="/lab7/lab7-5.html">Задание 5</a>	
-	</li>
-	<li>
-		<a href="/lab7/lab7-6.html">Задание 6</a>	
-	</li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Хасанов Р.М. ИВТ-417</title>
+</head>
+<body>
+	<h1>Лабораторная работа 7</h1>
+	<h2>Упражнения:</h2>
+
+	<ul>
+		<li>
+			<a href="/lab7/lab7-1.html">Задание 1</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-2.html">Задание 2</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-3.html">Задание 3</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-4.html">Задание 4</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-5.html">Задание 5</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-6.html">Задание 6</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-7.html">Задание 7</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-8.html">Задание 8</a>	
+		</li>
+		<li>
+			<a href="/lab7/lab7-9.html">Задание 9</a>	
+		</li>
+	</ul>
+
+</body>
+</html>
